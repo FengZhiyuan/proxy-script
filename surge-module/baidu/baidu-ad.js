d1c888fb88b8c072bce1d6bb82094e277c1ee7c1
冯志远 <fengzy21@gmail.com>
2023-02-24 17:56:18 +0800

    update：百度模块更新

--- surge-module/baidu/baidu-ad.js
+++ surge-module/baidu/baidu-ad.js
@@ -1,14 +1,57 @@
 // version: 1.0.0
+// 消息接口 http://c.tieba.baidu.com/c/u/follow/getFoldedMessageUserInfo
+// 预览图片接口 http://c.tieba.baidu.com/c/f/pb/picpage
+
+// version: 1.0.0
 const log = console.log.bind(console)
 
-let url = $request.url
-const body = JSON.parse($response.body)
+class Surge {
+    constructor() {
+        // 请求的 request
+        this.request = $request
+        // 请求的 response
+        this.response = $response
+    }
+    static new(...args) {
+        if (!this.instance) {
+            this.instance = new this(...args)
+        }
 
-// 时间线广告处理
-if (url.includes("weibointl")) {
-    body.data = []
+        return this.instance
+    }
+
+    urlFromApi() {
+        return this.request.url
+    }
+
+    bodyFromApi() {
+        return JSON.parse(this.response.body)
+    }
+
+    bodyToJson(bodyData) {
+        return JSON.stringify(bodyData)
+    }
+
+    // 结束请求
+    done(data) {
+        return $done({...data})
+    }
 }
 
-bodyJson = JSON.stringify(body)
+// 去除时间线广告
+const removePicturePageAd = (surge) => {
+    const body = surge.bodyFromApi()
+    delete body.thread
+    surge.done({ body: surge.bodyToJson(body) })
+}
 
-$done({ bodyJson })
+const __main = () => {
+    const surge = Surge.new()
+    const url = surge.urlFromApi()
+    // 删除图片预览广告
+    if (url.includes("/picpage")) {
+        removePicturePageAd(surge)
+    }
+}
+
+__main()
