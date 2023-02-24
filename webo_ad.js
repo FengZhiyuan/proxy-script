ae11c771f0c4810414a11db3c9e71a789880abcd
冯志远 <fengzy21@gmail.com>
2023-02-24 10:46:16 +0800

    update

--- webo_ad.js
+++ webo_ad.js
@@ -2,10 +2,10 @@
 const log = console.log.bind(console)
 
 let url = $request.url
-log("weibo_ad url is ", url)
 let body = $response.body
 
 if (url.includes("weibointl")) {
+    log("weibo_ad url is ", $request.url)
     let obj = JSON.parse(body)
     obj.data = []
     body = JSON.stringify(obj)
